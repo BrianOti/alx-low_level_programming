0937098227ec1a30e25709069dc2e3c4a277d7be
Brian Otieno <ourranous@gmail.com>
2022-07-07 07:56:36 -0700

    first push for functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks if letter is capitalised or not
+ * @c: int representing a character
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c isupper())
+	{
+		return (1);
+	}
+	return (0);
+}
