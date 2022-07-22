94aa31dc92680f3ea4711fd7a4dadd772ca260d2
Brian Otieno <ourranous@gmail.com>
2022-07-22 11:46:36 -0700

    core

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- *main - Entry point
- *@argc: count of the arguments supplied to the program
- *@argv: array of pointers to the strings
- *
- *Return: 0
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
  */
+
 
 int main(int argc, char *argv[])
 {
-int i;
-
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-return (0);
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
