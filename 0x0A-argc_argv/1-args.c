c11f2cb2df8c36207afb349dab8f589eaeffdedd
Brian Otieno <ourranous@gmail.com>
2022-07-22 12:31:39 -0700

    corre

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
