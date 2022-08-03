303d2dbc87df4eff967e23748ec8da0a01df44f2
Brian Otieno <ourranous@gmail.com>
2022-08-03 05:46:17 -0700

    problems

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 
 /**
- *print_name - print the name
- *@name: string
- *@f: function to print with
- *
- *Return: Void
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name != '\0' && f != '\0')
-f(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
